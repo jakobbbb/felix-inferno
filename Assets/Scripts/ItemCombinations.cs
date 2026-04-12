42b57d73dc32033281f57e0c3944e4778c0e8a1d
Jakob Ruckel <jakob@ruckel.de>
2026-04-12 15:01:09 +0200

    It's already perfect

--- Assets/Scripts/ItemCombinations.cs
+++ Assets/Scripts/ItemCombinations.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class ItemCombinations : MonoBehaviour {
 
     private string ItemCombination(string item_a, string item_b) {
-        if (item_a == "piranhaplant" && item_b == "tofu") {
-
+        if (item_a == "edding" && item_b == "gamejamposter") {
+            return "edding_gamejamposter";
         }
 
         return "nointeraction";
