f4e669f4c3da64a1902fd8d8f8b7a5752258a8c0
Sudorart <93275838+Sudorart@users.noreply.github.com>
2026-04-12 16:46:27 +0200

    Added Scriptabe Inventory Items

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,6 +5,7 @@ namespace BugGame.Inventory {
 [CreateAssetMenu(fileName = "Item", menuName = "Inventory Item", order = 0)]
 public class Item : ScriptableObject {
     public string displayName;
+        public string dialogueReference;
     public Sprite icon = null;
 
     public int GetId() {
